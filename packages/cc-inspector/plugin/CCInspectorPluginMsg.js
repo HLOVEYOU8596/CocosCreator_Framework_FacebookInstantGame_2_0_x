8c3da9c249c9d22df75e3d5beb356a41841de8b4
DESKTOP-0N2S7UO\zhang <zhangzhengbo@hotmail.com>
2018-10-25 20:03:52 +0800

    家里改动

--- packages/cc-inspector/plugin/CCInspectorPluginMsg.js
+++ packages/cc-inspector/plugin/CCInspectorPluginMsg.js
@@ -1,1 +1,1 @@
-module.exports={Msg:{Test:0,GetTreeInfo:1,GetNodeInfo:2,Active:100,Position:101,Rotation:102,Scale:103,Anchor:104,Size:105,Color:106,Opacity:107,Skew:108}};
+window.CCInspectorPluginMsg={Msg:{Test:0,GetTreeInfo:1,GetNodeInfo:2,Active:100,Position:101,Rotation:102,Scale:103,Anchor:104,Size:105,Color:106,Opacity:107,Skew:108}};
